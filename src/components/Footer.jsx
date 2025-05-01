9f79d262240b3a9cc7137cb1daee7c0bf6f852ca
Jesús Sánchez Cuevas <jsancue2103@gmail.com>
2025-05-01 18:43:56 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaGithub, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Footer = () => {
   return (
